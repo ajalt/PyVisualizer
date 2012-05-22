9c663c98704f9812b66c39bed3411adfb2b7e5ac
AJ Alt <ctrl.alt.aj@gmail.com>
2012-05-22 17:55:01 -0400

    Refactored some more functionality into the base class to improve performance.

--- visualizer.py
+++ visualizer.py
@@ -2,6 +2,7 @@
 
 import sys
 import random
+import time
 
 import numpy as np
 from PySide import QtCore, QtGui, QtMultimedia
@@ -32,15 +33,31 @@ class Visualizer(QtGui.QLabel):
         self.update_interval = update_interval #33ms ~= 30 fps
         self.sizeHint = lambda: QtCore.QSize(400, 400)
         self.setStyleSheet('background-color: black;');
+        self.setWindowTitle('PyVisualizer')
         
     def show(self):
         """Show the label and begin updating the visualization."""
         super(Visualizer, self).show()
-        self.generate()
+        self.refresh()
         
-    def generate(self):
+        
+    def refresh(self):
+        """Generate a frame, display it, and set queue the next frame"""
+        data = self.get_data()
+        interval = self.update_interval
+        if data is not None:
+            t1 = time.clock()
+            self.setPixmap(QtGui.QPixmap.fromImage(self.generate(data)))
+            #decrease the time till next frame by the processing tmie so that the framerate stays consistent
+            interval -= 1000 * (time.clock() - t1)
+        if self.isVisible():
+            QtCore.QTimer.singleShot(self.update_interval, self.refresh)
+        
+    def generate(self, data):
         """This is the abstract function that child classes will override to
-        draw a frame of the visualization."""
+        draw a frame of the visualization.
+        
+        The function takes an array of data and returns a QImage to display"""
         raise NotImplementedError()
     
     
@@ -98,79 +115,72 @@ class LineVisualizer(Visualizer):
                 elif QtCore.Qt.Key_1 <= event.key() <= QtCore.Qt.Key_9:
                     self.columns = event.key() - QtCore.Qt.Key_1 + 1
             
-    def generate(self):
-        data = self.get_data()
-        if data is not None:
-            fft = np.absolute(np.fft.rfft(data, n=len(data)))
-            freq = np.fft.fftfreq(len(fft), d=1./SAMPLE_RATE)
-            max_freq = abs(freq[fft == np.amax(fft)][0]) / 2
-            max_amplitude = np.amax(data)
-            
-            rect_width = int(self.width() / (self.columns * 2))
-            
-            freq_cap = 10000. #this determines the scale of lines
-            if max_freq >= freq_cap:
-                rect_height = self.height()
-            else:
-                rect_height = int(self.height() * max_freq / freq_cap)
-
-            
-            img = QtGui.QImage(self.width(), self.height(), QtGui.QImage.Format_RGB32)
-            img.fill(0) #black
-            
-            if rect_height >= 1:
-                painter = QtGui.QPainter(img)
-                painter.setPen(QtCore.Qt.NoPen)
-                painter.setBrush(self.brush) 
-                
-                for x in xrange(0, self.width() - rect_width, rect_width * 2):
-                    for y in xrange(0, self.height(), 2 * rect_height):
-                        if random.randint(0, int(max_amplitude / float(SAMPLE_MAX) * 10)):
-                            if self.display_evens:
-                                painter.drawRect(x, y, rect_width, rect_height)
-                            if self.display_odds:
-                                painter.drawRect(x + rect_width, self.height() - y - rect_height, rect_width, rect_height)
-                
-                del painter #
-                
-            self.setPixmap(QtGui.QPixmap.fromImage(img))
-        if self.isVisible():
-            QtCore.QTimer.singleShot(self.update_interval, self.generate)
-
-class Spectrogram(Visualizer):
-    def generate(self):
-        data = self.get_data()
+    def generate(self, data):
+        fft = np.absolute(np.fft.rfft(data, n=len(data)))
+        freq = np.fft.fftfreq(len(fft), d=1./SAMPLE_RATE)
+        max_freq = abs(freq[fft == np.amax(fft)][0])
+        max_amplitude = np.amax(data)
         
-        if data is not None:
-            fft = np.absolute(np.fft.rfft(data, n=len(data)))
-            freq = np.fft.fftfreq(len(fft), d=1./SAMPLE_RATE)
-            max_freq = abs(freq[fft == np.amax(fft)][0]) / 2
-            max_amplitude = np.amax(data)
+        rect_width = int(self.width() / (self.columns * 2))
+        
+        freq_cap = 20000. #this determines the scale of lines
+        if max_freq >= freq_cap:
+            rect_height = 1
+        else:
+            rect_height = int(self.height() * max_freq / freq_cap)
+            if rect_height == 2: rect_height = 1
             
-            bins = np.zeros(200)
-            #indices = (len(fft) - np.logspace(0, np.log10(len(fft)), len(bins), endpoint=False).astype(int))[::-1]
-            #for i in xrange(len(bins) - 1):
-            #    bins[i] = np.mean(fft[indices[i]:indices[i+1]]).astype(int)
-            #bins[-1] = np.mean(fft[indices[-1]:]).astype(int)
-            
-            step = int(len(fft) / len(bins))
-            for i in xrange(len(bins)):
-                bins[i] = np.mean(fft[i:i+step])
-                
-            img = QtGui.QImage(self.width(), self.height(), QtGui.QImage.Format_RGB32)
-            img.fill(0)
+        
+        img = QtGui.QImage(self.width(), self.height(), QtGui.QImage.Format_RGB32)
+        img.fill(0) #black
+        
+        if rect_height >= 1:
             painter = QtGui.QPainter(img)
             painter.setPen(QtCore.Qt.NoPen)
-            painter.setBrush(QtGui.QBrush(QtGui.QColor(255, 255, 255))) #white)
+            painter.setBrush(self.brush) 
             
-            for i, bin in enumerate(bins):
-                height = self.height() * bin / float(SAMPLE_MAX) / 10
-                width = self.width() / float(len(bins))
-                painter.drawRect(i * width, self.height() - height, width, height)
-                
-            del painter
+            for x in xrange(0, self.width() - rect_width, rect_width * 2):
+                for y in xrange(0, self.height(), 2 * rect_height):
+                    if random.randint(0, int(max_amplitude / float(SAMPLE_MAX) * 10)):
+                        if self.display_evens:
+                            painter.drawRect(x, y, rect_width, rect_height)
+                        if self.display_odds:
+                            painter.drawRect(x + rect_width, self.height() - y - rect_height, rect_width, rect_height)
             
-            self.setPixmap(QtGui.QPixmap.fromImage(img))
-        if self.isVisible():
-            QtCore.QTimer.singleShot(self.update_interval, self.generate)
+            del painter #
+            
+        return img
+
+class Spectrogram(Visualizer):
+    def generate(self, data):
+        fft = np.absolute(np.fft.rfft(data, n=len(data)))
+        freq = np.fft.fftfreq(len(fft), d=1./SAMPLE_RATE)
+        max_freq = abs(freq[fft == np.amax(fft)][0]) / 2
+        max_amplitude = np.amax(data)
+        
+        bins = np.zeros(200)
+        #indices = (len(fft) - np.logspace(0, np.log10(len(fft)), len(bins), endpoint=False).astype(int))[::-1]
+        #for i in xrange(len(bins) - 1):
+        #    bins[i] = np.mean(fft[indices[i]:indices[i+1]]).astype(int)
+        #bins[-1] = np.mean(fft[indices[-1]:]).astype(int)
+        
+        step = int(len(fft) / len(bins))
+        for i in xrange(len(bins)):
+            bins[i] = np.mean(fft[i:i+step])
+            
+        img = QtGui.QImage(self.width(), self.height(), QtGui.QImage.Format_RGB32)
+        img.fill(0)
+        painter = QtGui.QPainter(img)
+        painter.setPen(QtCore.Qt.NoPen)
+        painter.setBrush(QtGui.QBrush(QtGui.QColor(255, 255, 255))) #white)
+        
+        for i, bin in enumerate(bins):
+            height = self.height() * bin / float(SAMPLE_MAX) / 10
+            width = self.width() / float(len(bins))
+            painter.drawRect(i * width, self.height() - height, width, height)
+            
+        del painter
+        
+        return img
+        
         
